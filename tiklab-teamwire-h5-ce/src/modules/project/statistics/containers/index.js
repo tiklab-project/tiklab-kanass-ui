900e37dd422940760caa90d15da3917d651f1c04
yuanjiexuan <yuanjiexuan@darthcloud.com>
2023-02-27 19:43:00 +0800

    修改文件名称，改成大驼峰

--- tiklab-teamwire-h5-ce/src/modules/project/statistics/containers/index.js
+++
@@ -1,8 +0,0 @@
-import Statistics from "../components/statistics";
-import {StatisticStore, STATISTICS_STORE} from "../store/statisticStore";
-
-export {
-    Statistics,
-    StatisticStore,
-    STATISTICS_STORE
-}
